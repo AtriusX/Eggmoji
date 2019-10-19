feb1cc79a756432c83488cffc437147eb8f3780d
Atrius <atriusedm@gmail.com>
2019-10-18 20:08:10 -0400

    Template metadata

---
+++ templates/meta.js
@@ -0,0 +1,105 @@
+meta = {
+    base: "./templates/images/base.png",
+    shadow: "./templates/images/shadow.png",
+    mask: "./templates/images/mask.png",
+    templates: [
+        {
+            src: "./templates/images/line_1.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/line_2.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/line_3.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/line_4.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/pattern_1.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/pattern_2.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/stripes_1.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/stripes_2.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/stripes_3.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/texture_1.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/texture_2.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/texture_3.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/texture_4.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        },
+        {
+            src: "./templates/images/texture_5.png",
+            max_rotation: 180,
+            flippable: true,
+            stretching: 0,
+            words: ""
+        }
+    ]
+};
