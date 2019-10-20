e8038d7f432eb560689e838cf0898cc07447fd83
Atrius <atriusedm@gmail.com>
2019-10-19 21:27:10 -0400

    Updates

---
+++ extentions.js
@@ -0,0 +1,68 @@
+CanvasRenderingContext2D.prototype.drawImg = function(img, angle, opacity) {
+    this.save();
+    this.translate(CANVAS_CENTER, CANVAS_CENTER);
+    this.rotate(angle * Math.PI / 180);
+    this.globalAlpha = opacity;
+    this.drawImage(img, -CANVAS_CENTER, -CANVAS_CENTER);
+    this.restore();
+}
+
+/**
+ * Generates a numeric seed from a string input.
+ * 
+ * @param {String} input The seed source.  
+ */
+String.prototype.seed = function(len) {
+    // Map each character to it's corresponding charcode
+    let seed = this.split('')
+       .map(c => parseInt(c.charCodeAt(0), 10))
+       .join('');
+    // Prevent an infinite loop
+    if (!seed) {
+        return '0';
+    }
+    // Normalize seed length to set value
+    while (seed.length < len) {
+        seed += seed.split('').reverse().join('');
+    }
+    // Remove characters until the seed matches the expected length
+    let dist = Math.floor(len / (seed.length - len));
+    for (let i = dist; seed.length > len; i += dist) {
+        seed = seed.substring(0, i) + seed.substring(i + 1);
+    }
+    return seed;
+}
+
+Image.prototype.color = function(c) {
+    let [ canvas, ctx ] = getCanvas();
+    ctx.drawImage(this, 0, 0);
+    ctx.globalCompositeOperation = "source-in";
+    ctx.fillStyle = c ? c : `hsl(${Math.random() * 360}, 100%, 75%)`
+    // '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    ctx.globalCompositeOperation = "source-over";
+    return canvas;
+}
+
+HTMLCanvasElement.prototype.clear = function() {
+    this.getContext('2d')
+        .clearRect(0, 0, this.width, this.height);
+}
+
+function createImage(url) {
+    let img = new Image();
+    img.src = url;
+    return img;
+}
+
+function getCanvas(id) {
+    let canvas; 
+    if (id) {
+        canvas = document.getElementById(id);
+    } else {
+        canvas = document.createElement('canvas');
+        canvas.height = CANVAS_SIZE;
+        canvas.width = CANVAS_SIZE;
+    }
+    return [canvas, canvas.getContext('2d')];
+}
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const IMG_DATA      = [];
 TEMPLATE_META.forEach(img => {
     IMG_DATA.push(createImage(img.src));
 });
-
+// 
 const IMG_BASE   = createImage(meta.base);
 const IMG_MASK   = createImage(meta.mask);
 const IMG_SHADOW = createImage(meta.shadow);
@@ -15,84 +15,23 @@ const IMG_SHADOW = createImage(meta.shadow);
 function process() {
     let [ canvas, ctx ] = getCanvas('egg');
     // Reset canvas state
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // Get name
-    let name  = document.getElementById('name').value;
+    canvas.clear();
     // Generate seed
-    let seed = genSeed(name, SEED_LENGTH);
-    // Tests
-    drawImg(ctx, IMG_BASE, 0, 1);
+    let name = document.getElementById('name').value;
+    let seed = name.seed(SEED_LENGTH);
+    Math.seedrandom(seed);
+    ctx.drawImg(IMG_BASE.color(), 0, 1);
     IMG_DATA.forEach(img => { 
-        if (Math.random() > 0.5) {
-            drawImg(ctx, color(img), Math.random() * 180, Math.random());
-        }
+        if (Math.random() > 0.8)
+            ctx.drawImg(img.color(), Math.random() * 180, Math.random());
     });
-    drawImg(ctx, IMG_SHADOW, 0, 0.15);
-    drawImg(ctx, IMG_MASK, 0, 1);
-}
-
-/**
- * Generates a numeric seed from a string input.
- * 
- * @param {String} input The seed source.  
- */
-function genSeed(input, len) {
-    // Map each character to it's corresponding charcode
-    let seed = input.split('')
-       .map(c => parseInt(c.charCodeAt(0), 10))
-       .join('');
-    // Normalize seed length to set value
-    while (seed.length < len) {
-        seed += seed.split('').reverse().join('');
-    }
-    // Remove characters until the seed matches the expected length
-    let dist = Math.floor(len / (seed.length - len));
-    for (let i = dist; seed.length > len; i += dist) {
-        seed = seed.substring(0, i) + seed.substring(i + 1);
-    }
-    return seed;
+    ctx.drawImg(IMG_SHADOW, 0, 0.18);
+    ctx.drawImg(IMG_MASK.color('#F5F5F5'), 0, 1);
 }
 
 function download() {
-    let [ canvas, context ] = getCanvas('egg');
-    let name    = document.getElementById('name');
-    
+    let [ canvas, ctx ] = getCanvas('egg');
+    // let name  = document.getElementById('name');
     let input = name.value;
+    canvas.toDataURL('image/png');
 }
-
-function drawImg(ctx, img, angle, opacity) {
-    ctx.save();
-    ctx.translate(CANVAS_CENTER, CANVAS_CENTER);
-    ctx.rotate(angle * Math.PI / 180);
-    ctx.globalAlpha = opacity;
-    ctx.drawImage(img, -CANVAS_CENTER, -CANVAS_CENTER);
-    ctx.restore();
-}
-
-function createImage(url) {
-    let img = new Image();
-    img.src = url;
-    return img;
-}
-
-function color(img) {
-    let [ canvas, ctx ] = getCanvas();
-    ctx.drawImage(img, 0, 0);
-    ctx.globalCompositeOperation = "source-in";
-    ctx.fillStyle = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    ctx.globalCompositeOperation = "source-over";
-    return canvas;
-}
-
-function getCanvas(id) {
-    let canvas; 
-    if (id) {
-        canvas = document.getElementById(id);
-    } else {
-        canvas = document.createElement('canvas');
-        canvas.height = CANVAS_SIZE;
-        canvas.width = CANVAS_SIZE;
-    }
-    return [canvas, canvas.getContext('2d')];
-} 
